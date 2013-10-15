2967f462f06fad6a24f6c46dc7f1e59ad9400ed9
IGZangelsanchez <angel.sanchez@intelygenz.com>
2013-10-15 17:50:42 +0200

    Code formatting

--- src/server/lib/util/mongodb.js
+++ src/server/lib/util/mongodb.js
@@ -1,7 +1,7 @@
 
 var MongoClient = require('mongodb').MongoClient,
-    config = require('../util/config').DB,
-    logger = require('../util/logger')(__filename);
+		config = require('../util/config').DB,
+		logger = require('../util/logger')(__filename);
 
 /**
  * MongoDB database
@@ -10,46 +10,46 @@ var db, pending = [], opening = false;
 
 
 function flushPending (err, db) {
-  for ( f = 0, F = pending.length; f < F; f++ ) {
-    pending[f](err, db);
-  }
-  
-  pending = [];
+	for ( f = 0, F = pending.length; f < F; f++ ) {
+		pending[f](err, db);
+	}
+	
+	pending = [];
 }
 
 function getMongoDB (callback) {
-  if ( db ) {
-    return callback(null, db);
-  }
+	if ( db ) {
+		return callback(null, db);
+	}
 
-  pending.push(callback);
-  
-  if ( opening ) {
-    return;
-  }
+	pending.push(callback);
+	
+	if ( opening ) {
+		return;
+	}
 
-  opening = true;
+	opening = true;
 
-  // The primary committer to node-mongodb-native says:
-  //  "You open do MongoClient.connect once when your app boots up and reuse the db object.
-  //   It's not a singleton connection pool each .connect creates a new connection pool."
-  MongoClient.connect(config.CONN, function(err, database) {
-    var f, F;
+	// The primary committer to node-mongodb-native says:
+	//  "You open do MongoClient.connect once when your app boots up and reuse the db object.
+	//   It's not a singleton connection pool each .connect creates a new connection pool."
+	MongoClient.connect(config.CONN, function(err, database) {
+		var f, F;
 
-    opening = false;
+		opening = false;
 
-    if (err) {
-      logger.error("MongoClient.connect: " + err);
+		if (err) {
+			logger.error("MongoClient.connect: " + err);
 
-      flushPending(err, null);
-      return;
-    }
+			flushPending(err, null);
+			return;
+		}
 
-    db = database;
-    logger.info('Mongodb connection established');
+		db = database;
+		logger.info('Mongodb connection established');
 
-    flushPending(null, db);
-  });
+		flushPending(null, db);
+	});
 
 }
 
