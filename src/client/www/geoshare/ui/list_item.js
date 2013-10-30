63e9b75edec6c1dd388ac655bd9df0f935912e04
surtich <surtich@gmail.com>
2013-10-30 01:07:21 +0100

    Added WebIntent plugin to be able to start Hangouts with Cordova. In mobile app, when press the Hangout button, it creates a new Google+ Intent (for now, it seems that it is not possible to start a hangout directly).

--- src/client/www/geoshare/ui/list_item.js
+++ src/client/www/geoshare/ui/list_item.js
@@ -11,10 +11,35 @@ iris.ui(function(self) {
 			email: contact.email || "???",
 			emailto: contact.email ? 'mailto:' + contact.email : '',
 			photo: contact.photo ? contact.photo : '',
-			distance: isNumber(contact.distance) ? iris.number(contact.distance) : '???',
-			hangout: 'https://plus.google.com/hangouts/_?gid=' + geosharecfg.google_project_id
+			distance: isNumber(contact.distance) ? iris.number(contact.distance) : '???'
 		});
+		
+		var link = self.get('hangout');
+		if (geoshare.isBrowser) {
+			link.attr('href', 'https://plus.google.com/hangouts/_?gid=' + geosharecfg.google_project_id);
+		} else {
+			link.click(function() {
+				mobileHangout(contact);
+				return false;
+			});	
+		}
 	};
+
+	function mobileHangout(contact) {
+		CDV.WEBINTENT.startActivity(
+			{
+				action: CDV.WEBINTENT.ACTION_VIEW,
+				url: 'https://plus.google.com/' + contact._id
+			},
+			function() {
+				iris.log('Started Google+ for ' + contact._id);
+			},
+			function(err) {
+				iris.notify('notify', {msg: 'Error: cannot open Google+'});
+				iris.log('Error: cannot open Google', err);
+			}
+		);
+	}
 
 	function isNumber(n) {
 		return !isNaN(parseFloat(n)) && isFinite(n);
